a912cb4742b5e3f5d1180b464ce74bb66ddbbc62
Fabrizio Salerno <fabrizio.salerno@cern.ch>
2025-10-31 18:20:52 +0100

    removing old and unnecessary files

--- python/postprocessing/PF_eval/runner.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash 
-cd /afs/cern.ch/user/f/fsalerno/CMSSW_13_2_11/src/PhysicsTools/NanoAODTools/python/postprocessing/PF_eval
-cmsenv
-export XRD_NETWORKSTACK=IPv4
-python3 PF_eval.py -dat $1 -final_name $2 -outname $3 -label $4
-echo $3 ->files_names.txt 
